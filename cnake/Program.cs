8e7f6fc3f5b7a39189d0537f16797b0a59907d06
IlenaCaruk <locus93@bk.ru>
2019-07-20 00:04:01 +0300

    Процедурный подход. Функция Draw

--- cnake/Program.cs
+++ cnake/Program.cs
@@ -10,8 +10,24 @@ namespace cnake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello world");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
+            Draw(x1, y1, sym1);
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+            Draw(x2, y2, sym2);
+
+
             Console.ReadKey();
+        }
+        //скрывает подробности реализации
+        static void Draw(int x, int y, char sym)//принимает в качестве аргумента входные параметры
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+
         }
     }
 }
