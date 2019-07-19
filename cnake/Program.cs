41a472662d840d8b8e38ef8bc727edc534c3a434
IlenaCaruk <locus93@bk.ru>
2019-07-20 01:55:52 +0300

    Создан класс Point

--- cnake/Program.cs
+++ cnake/Program.cs
@@ -10,24 +10,35 @@ namespace cnake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
+            Point p2 = new Point();
+            p2.y = 4;
+            p2.x = 5;
+            p2.sym = '#';
+            p2.Draw();
+            
+            /*int x1 = 1;
             int y1 = 3;
             char sym1 = '*';
-            Draw(x1, y1, sym1);
-            int x2 = 4;
+            Draw(x1, y1, sym1);*/
+            /*int x2 = 4;
             int y2 = 5;
             char sym2 = '#';
-            Draw(x2, y2, sym2);
+            Draw(x2, y2, sym2);*/
 
 
             Console.ReadKey();
         }
         //скрывает подробности реализации
-        static void Draw(int x, int y, char sym)//принимает в качестве аргумента входные параметры
+        /*static void Draw(int x, int y, char sym)//принимает в качестве аргумента входные параметры
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
 
-        }
+        }*/
     }
 }
